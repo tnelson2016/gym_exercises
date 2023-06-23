2f3bbb4b14cdcd21605dca773793d877a0c71464
tnelson2016 <troy.nelson@bracari.com>
2023-06-23 17:29:01 -0400

    start of navbar styling

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Box, Stack, Typography} from '@mui/material'
+
+const HeroBanner = () => {
+  return (
+    <Box>
+      <Typography
+        color="#FF2625"
+        fontWeight="600"
+        fontSize="26px">
+        Fitness Club
+      </Typography>
+    </Box>
+  )
+}
+
+export default HeroBanner
